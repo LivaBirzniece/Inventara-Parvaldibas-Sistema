7cae1d415a62193e2c22f055428952f5e7a8da7f
Līva <birznieceliva@gmail.com>
2025-12-03 10:48:02 +0200

    Izveidots pamata GUI ekrāns

--- src/InventaraParvaldibasSistema/IPS.java
+++ src/InventaraParvaldibasSistema/IPS.java
@@ -1,9 +1,30 @@
 package InventaraParvaldibasSistema;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class IPS {
+	
+	public IPS() {
+
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(400,400,400,400));
+		panel.setLayout(new GridLayout(0,1));
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Inventara Parvaldibas Sistema");
+		frame.pack();
+		frame.setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		
+		new IPS();
 
 	}
 
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module InventaraParvaldibasSistema {
+	requires java.desktop;
 }
