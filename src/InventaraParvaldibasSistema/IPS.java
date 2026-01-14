5d09cd467b310b0a2ece006eee81aa7ebedc2e70
Līva <birznieceliva@gmail.com>
2026-01-14 09:24:26 +0200

    Pievienota opcija kārtot datus  A-Z, Z-A

--- src/InventaraParvaldibasSistema/IPS.java
+++ src/InventaraParvaldibasSistema/IPS.java
@@ -1,11 +1,33 @@
 package InventaraParvaldibasSistema;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 import java.io.FileWriter;
 import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 
 public class IPS {
     private JFrame frame;
@@ -172,6 +194,9 @@ public class IPS {
         if(editMode) editTableModel = m; else viewTableModel = m;
 
         JTable t = new JTable(m);
+        TableRowSorter<DefaultTableModel> sorter =
+                new TableRowSorter<>(m);
+        t.setRowSorter(sorter);
         t.setRowHeight(50);
         t.setBackground(ModernsDizains.BG_DARK);
         t.setFillsViewportHeight(true);
